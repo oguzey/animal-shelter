60385ba4a40fef4f157703824a406ff87a6198aa
Oleh Huzei <oleg.guz.rus@gmail.com>
2016-06-01 09:48:59 +0300

    Added and configured http-server on mono

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-docker network create my-network || exit -1
+docker network create --subnet=172.18.0.0/16 my-network || exit -1
+
+
+#cd simple-http-server && docker build -t test-server . && docker run -d --name server-handler --net=my-network --net-alias=server-handler-net test-server
+cd main-server 
+docker build -t mono-http-server:1.0 . || exit -1
+docker run -d --name http-server --net=my-network --ip 172.18.0.20 --add-host myproject:172.18.0.20 --add-host myhost:172.18.0.20 mono-http-server:1.0 || exit -1
+docker run -d --name http-server2 --net=my-network --ip 172.18.0.21 --add-host myproject:172.18.0.21 --add-host myhost:172.18.0.21  mono-http-server:1.0 || exit -1
+cd ..
+
 docker build -t web-nginx:1.10 . || exit -1 
-docker run --name web-server -p 80:80 -d --net=my-network --net-alias=web-server-net web-nginx:1.10 || exit -1
-cd simple-http-server && docker build -t test-server . && docker run -d --name server-handler --net=my-network --net-alias=server-handler-net test-server
+docker run --name web-server -p 80:80 -d --net=my-network --ip 172.18.0.22 web-nginx:1.10 || exit -1
--- stop.sh
+++ stop.sh
@@ -2,8 +2,14 @@ docker stop web-server
 docker rm web-server
 docker rmi web-nginx:1.10
 
-docker stop server-handler
-docker rm server-handler
-docker rmi test-server
+#docker stop server-handler
+#docker rm server-handler
+#docker rmi test-server
+
+docker stop http-server
+docker rm http-server
+docker stop http-server2
+docker rm http-server2
+docker rmi mono-http-server:1.0
 
 docker network rm my-network
