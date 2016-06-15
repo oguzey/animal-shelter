08bdab7e91eb21d74e6f043c8c604a076604a71b
Oleh Huzei <oleg.guz.rus@gmail.com>
2016-06-16 02:45:59 +0300

    Update scripts

--- load-data-to-db.sh
+++ load-data-to-db.sh
@@ -1,2 +1,2 @@
-docker-compose exec cassandra-server cqlsh -f /scripts/create.cql 
-docker-compose exec cassandra-server cqlsh -f /scripts/insert.cql
+docker-compose exec cassandra-server0 cqlsh -f /scripts/create.cql 
+docker-compose exec cassandra-server0 cqlsh -f /scripts/insert.cql
